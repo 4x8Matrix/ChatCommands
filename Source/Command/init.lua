1ad2e74db75a7db20080f3487ad69c752847f48f
AsynchronousMatrix <catbob614@gmail.com>
2023-05-31 19:04:53 +0100

    first commit

---
+++ Source/Command/init.lua
@@ -0,0 +1,18 @@
+-- // Dependencies
+local Signal = require(script.Parent.Signal)
+
+-- // Module
+local Command = { }
+
+Command.Interface = { }
+Command.Prototype = { }
+
+function Command.Prototype:Execute()
+
+end
+
+function Command.Interface.new()
+
+end
+
+return Command.Interface
---
+++ Source/init.lua
@@ -0,0 +1,10 @@
+-- // Dependencies
+local Signal = require(script.Parent.Signal)
+
+-- // Module
+local CommandO = { }
+
+CommandO.Interface = { }
+CommandO.Interface.Command = require(script.Command)
+
+return CommandO.Interface
