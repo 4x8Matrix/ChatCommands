e00316bf776fcfb12028b879d559c6dd1f78da7d
AsynchronousMatrix <catbob614@gmail.com>
2023-06-04 02:06:07 +0100

    feature: update test 'Development' script

---
+++ Build/Source/Development.server.lua
@@ -0,0 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ChatCommands = require(ReplicatedStorage.Packages.ChatCommands)
+
+ChatCommands.Command.new("Test", function(player, ...)
+	warn(player, ...)
+end)
+	:SetDescription("Test Command!")
+	:AddAlias("A")
+
+ChatCommands:Start()
